2d136e8ab7a3953a47f39d0999b8c981f398dd99
lsw <809568994@qq.com>
2022-01-04 10:26:42 +0800

    1.4

--- src/views/4-watch.vue
+++ src/views/4-watch.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, watch, reactive } from "vue";
+import { defineComponent, watch, reactive, toRef } from "vue";
 
 export default defineComponent({
   name: "",
@@ -15,17 +15,25 @@ export default defineComponent({
       name: "小白",
       age: 18,
     });
+
     watch(
       state, //  注意: watch只能侦听响应式数据，state.name不算响应式数据，需要使用函数格式才能监听响应式对象的属性
       (n, o) => {
         //这两个参数也可以不写，第一个是变化后的新数据，第二个是原数据
-        console.log(n, o);
+        console.log("旧的值1: ", o, "----", "新的值: ", n);
       }
     );
     watch(
-      () => state.age, //  监听对象中的属性时，使用函数格式，return对象属性
+      () => state.age, // 监听对象中的属性，方法1，使用函数格式，return对象属性
       (n, o) => {
-        console.log("旧的值: ", o, "----", "新的值: ", n);
+        console.log("旧的值2: ", o, "----", "新的值: ", n);
+      }
+    );
+    let name = toRef(state, "name");
+    watch(
+      name, // 监听对象中的属性，方法2，使用toRef，将指定属性转换为ref格式的响应式数据，然后就可以监听了，不需要加.value
+      (n, o) => {
+        console.log("旧的值3: ", o, "----", "新的值: ", n);
       }
     );
     watch(
@@ -36,8 +44,8 @@ export default defineComponent({
       }
     );
     const update = () => {
-      state.name += "改变";
       state.age += 1;
+      state.name += "改变";
     };
     return {
       state,
--- src/views/9-toRefs.vue
+++ src/views/9-toRefs.vue
@@ -3,7 +3,7 @@
   <h />
   <h3>name: {{ name }}</h3>
   <h3>age: {{ age }}</h3>
-
+  <button @click="change">改变</button>
 </template>
 
 <script lang='ts'>
@@ -16,10 +16,23 @@ export default defineComponent({
       name: "小白",
       age: 24,
     });
-    // 使用reactive创建的响应式对象，使用state.xx来操作单个属性，有点臃肿，可以使用...扩展运算符，把属性全部解构出来
-    // 直接使用...解构reactive，解构出来的属性会失去响应式，此时需要使用toRefs
+
+    let x = toRefs(state); // 返回一个对象，对象中的每个属性都是ref格式的响应式数据，可以使用解构赋值单独取出，也可以使用...全部一次性取出
+    let { age, name } = x; // 解构赋值，单独取出想要使用的属性
+    console.log(x);
+    console.log(age.value);
+    const change = () => {
+      age.value += age.value; // 第一种，使用toRefs转换后，可以直接修改age和name，但是需要加上.value(因为toRef是将响应式对象中的属性全部转换为ref格式)
+      name.value += name.value;
+      // state.name += state.name; // 第二种:使用这个方式也可以改变响应式数据state中的name和age
+      // state.age += state.age;
+    };
     return {
-      ...toRefs(state), // 这样就可以直接在模板中使用name，age了，表示先使用toRefs把目标对象的每个属性都转换成ref格式的响应式数据，然后使用...扩展运算符把对象每个属性都提取出来
+      // age, // 第一种方法，使用解构赋值单独指定数据
+      // name,
+      change,
+      ...toRefs(state), // 第二种方式使用...扩展运算符取出全部数据
+      // reactive直接使用...扩展运算符，解构出来的属性会失去响应式，此时需要使用toRefs
     };
   },
 });
